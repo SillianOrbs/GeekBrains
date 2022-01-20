c1ca00fd853e5d518909047eb622962e4a7f3639
Nai <88776801+SillianOrbs@users.noreply.github.com>
2022-01-20 11:35:33 +0300

    Отрисовка котика по 1му уроку

---
+++ University/Java1/lesson_1/MainApp.java
@@ -0,0 +1,18 @@
+package Java1.lesson_1;
+
+public class MainApp {
+    public static void main(String[] args) {
+        drawCat();
+    }
+    public static void drawCat() {
+        System.out.println("           _..---...,\"\"-._     ,/}/)\n" +
+                "        .''        ,      ``..'(/-<\n" +
+                "       /   _      {      )         \\\n" +
+                "      ;   _ `.     `.   <         a(\n" +
+                "    ,'   ( \\  )      `.  \\ __.._ .: y\n" +
+                "   (  <\\_-) )'-.____...\\  `._   //-'\n" +
+                "    `. `-' /-._)))      `-._)))\n" +
+                "      `...'         ");
+    }
+}
+
